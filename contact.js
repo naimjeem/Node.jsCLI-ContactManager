19b7d2fed58bfdc12bc680ab59bf6b1b825d572e
Naim Jeem <naim36208@gmail.com>
2017-08-24 17:23:42 +0600

    Inquery data added

--- contact.js
+++ contact.js
@@ -1,6 +1,30 @@
 const program = require('commander');
+const { prompt } = require('inquirer'); 
 // Require logic.js file and extract controller functions using JS destructuring assignment
 const { addContact, getContact } = require('./logic');
+
+const questions = [
+  {
+    type : 'input',
+    name : 'firstname',
+    message : 'Enter firstname ...'
+  },
+  {
+    type : 'input',
+    name : 'lastname',
+    message : 'Enter lastname ...'
+  },
+  {
+    type : 'input',
+    name : 'phone',
+    message : 'Enter phone number ...'
+  },
+  {
+    type : 'input',
+    name : 'email',
+    message : 'Enter email address ...'
+  }
+];
 
 program
   .version('0.0.1')
